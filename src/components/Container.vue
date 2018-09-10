9fad17976dfbf9fb6f5fff3efb8352804ef3bf80
Shao Xiang Sum <shaoxiangsum@Shaos-MacBook-Pro.local>
2018-09-10 10:51:58 +0200

    initial commit

---
+++ src/components/Container.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="container">
+    <h1 id= "message">{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+#container {
+  background: skyblue;
+}
+#message {
+  text-align: center;
+  color: #ffffff;
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 5px;
+}
+</style>
