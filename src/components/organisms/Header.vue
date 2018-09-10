bdd2ab975c8195ecb4f2903f5f72d5addf4c19e3
Aaron Sum <sumsx03@gmail.com>
2018-09-10 14:45:50 +0200

    add header (#1)

---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="header">
+        <Title id="title" message-on-left="To" message-on-right="Do"/>
+    </div>
+</template>
+
+<script>
+import Title from '../molecules/Title.vue'
+
+export default {
+  name: 'Header',
+  components: {
+    Title
+  }
+}
+</script>
+
+<style >
+#header{
+    color: #FFFFFF;
+    background-color: skyblue;
+    padding: 5px;
+    text-align: center;
+}
+</style>
