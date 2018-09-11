626d36dbebd287229c3d387b9a5c43ba9aa60970
Shao Xiang Sum <shaoxiangsum@Shaos-MacBook-Pro.local>
2018-09-11 17:15:27 +0200

    add task summary message

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
     <div class="app">
         <Header/>
         <Body/>
+        <Summary/>
     </div>
 </template>
 
 <script>
 import Header from './components/pages/Header.vue'
 import Body from './components/pages/Body.vue'
+import Summary from './components/atoms/SummaryMessage.vue'
 
 export default {
   name: 'app',
   components: {
     Header,
-    Body
+    Body,
+    Summary
   }
 }
 </script>
